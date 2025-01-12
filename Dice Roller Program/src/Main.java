1cf8b3bab9c00179a794b250b4da616973e54451
Nischal Dhoj Kunwar <kunwarnischal3@gmail.com>
2025-01-12 21:23:26 +0545

    created dice roller program using  random numbers, asci art, static method and enhanced switch

--- Dice Roller Program/src/Main.java
+++ Dice Roller Program/src/Main.java
@@ -19,16 +19,79 @@ public class Main {
         if(numOfDice > 0){
             for(int i =0; i < numOfDice; i++){
                 int roll = random.nextInt(1,7);
+                printDie(roll);
                 System.out.println("You rolled: " + roll);
-                total += roll;                     //total + roll;
-                System.out.println("The ");
+                total += roll;                     //total + roll
             }
+            System.out.println("\nTotal: " + total);
         }
         else {
             System.out.println("Number of dice to be rolled must be greater than 0!");
         }
-
         scanner.close();
+    }
+
+    static void printDie(int roll){
+
+        // for bullet point, win + r and then type charmap and select bullet point
+        String dice1 = """      
+                 -------
+                |       |
+                |   ●   |
+                |       |
+                 -------
+                """;
+        // triple quote for multi-line string
+
+        String dice2 = """
+                 -------
+                | ●     |
+                |       |
+                |     ● |
+                 -------
+                """;
+
+        String dice3 = """
+                 -------
+                | ●     |
+                |   ●   |
+                |     ● |
+                 -------
+                """;
+
+        String dice4 = """
+                 -------
+                | ●   ● |
+                |       |
+                | ●   ● |
+                 -------
+                """;
+
+        String dice5 = """
+                 -------
+                | ●   ● |
+                |   ●   |
+                | ●   ● |
+                 -------
+                """;
+
+        String dice6 = """
+                 -------
+                | ●   ● |
+                | ●   ● |
+                | ●   ● |
+                 -------
+                """;
+
+        switch (roll){
+            case 1 -> System.out.print(dice1);
+            case 2 -> System.out.print(dice2);
+            case 3 -> System.out.print(dice3);
+            case 4 -> System.out.print(dice4);
+            case 5 -> System.out.print(dice5);
+            case 6 -> System.out.print(dice6);
+            default -> System.out.print("Invalid Roll!");
+        }
 
     }
 }
